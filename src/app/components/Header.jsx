9f510babceefef66b1e80ccbc2049b535255ee81
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-10-14 21:59:24 -0300

    feat: adiciona navegação no Header e cria template da página de histórico

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import Geral from './Geral'
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { IoMdClose } from "react-icons/io";
 
 export default function Header() {
+  const [openHeader, setOpenHeader] = useState(false)
+  const router = useRouter();
+
   return (
-    <div className='w-screen h-[65px] md:h-[85px] bg-primary'>
+    <div className='fixed top-0 left-0 w-screen h-[65px] md:h-[85px] bg-primary'>
+      <div className='flex items-center h-full w-full px-5'>
+        {openHeader === false ? (
+          <FaBars className='text-white' size={20} onClick={() => setOpenHeader(true)} />
+        ) : (
+          <IoMdClose className='text-white' size={25} onClick={() => setOpenHeader(false)} />
+        )}
+      </div>
+      {openHeader && (
+        <div className='w-full bg-primary mt-[-1px] text-white flex flex-col items-center'>
+          <p className='py-2 w-full text-center hover:bg-blue-950' onClick={() => router.push('/')}>Visão geral</p>
+          <p className='py-2 w-full text-center hover:bg-blue-950' onClick={() => router.push('/historico')}>Histórico de transações</p>
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/app/historico/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function page() {
+  return (
+    <div>
+      <Header />
+      <main>
+        
+      </main>
+    </div>
+  )
+}
